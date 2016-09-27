92cf4dd12800ee4a1c4eb4ff63d4e31ca42a1949
Dan Levinson <danholandi@gmail.com>
2016-09-27 13:31:52 +0300

    update

--- javascript/script.js
+++ javascript/script.js
@@ -5,8 +5,6 @@ alert("We hope you'll enjoy this simple game.");
 var number = Math.floor((Math.random() * 100) + 1);
 var guess;
 
-do {
-guess = parseInt(prompt("Guess a number:",guess));
+while(guess != number){
 
 }
-while(guess != number)
