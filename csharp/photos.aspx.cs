7d6a25a8da2d57766c72791723f4e8c40d74f5e3
Dan Levinson <danholandi@gmail.com>
2017-01-03 10:55:49 +0200

    add photos

---
+++ csharp/photos.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class csharp_photos : System.Web.UI.Page
+{
+
+    public string str = "";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+
+
+
+
+    }
+}
