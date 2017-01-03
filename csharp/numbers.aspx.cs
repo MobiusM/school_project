02a1b85320c91c14d9d721f20af0366f291c6120
Dan Levinson <danholandi@gmail.com>
2017-01-03 10:30:06 +0200

    update

---
+++ csharp/numbers.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class csharp_numbers : System.Web.UI.Page
+{
+
+
+    public string str = "";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        for (int i = 1; i <= 10; i++)
+        {
+           
+            if (i == 6)
+            {
+                str += "<br>";
+            }
+
+            str += i;
+
+        }
+
+    }
+}
--- csharp/testing.aspx.cs
+++ csharp/testing.aspx.cs
@@ -8,10 +8,21 @@ using System.Web.UI.WebControls;
 public partial class csharp_testing : System.Web.UI.Page
 {
 
-
+    public string str = "";
 
     protected void Page_Load(object sender, EventArgs e)
     {
+
+        int width = 300;
+
+        string[] colors = { "red", "blue", "yellow", "green", "aqua", "orange", "pink", "olive", "silver", "black" };
+
+        for (int i = 0; i < colors.Length; i++)
+        {
+            str += "<hr width = " + width + " color = " + colors[i] + "/>";
+            width -= 20;
+
+        }
 
     }
 }
