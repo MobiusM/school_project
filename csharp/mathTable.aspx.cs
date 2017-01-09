1cea9139969389a8823ecf6ffc264a921f9e5299
Dan Levinson <danholandi@gmail.com>
2017-01-09 12:48:55 +0200

    add math table

---
+++ csharp/mathTable.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class csharp_mathTable : System.Web.UI.Page
+{
+
+    public string str = "";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+
+
+    }
+}
