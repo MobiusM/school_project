696cf01b18e7672bb6d0116dc7c4cc7da8b1ef5a
Dan Levinson <danholandi@gmail.com>
2017-01-09 12:48:43 +0200

    change to proper reference

--- csharp/photos.aspx.cs
+++ csharp/photos.aspx.cs
@@ -13,11 +13,21 @@ public partial class csharp_photos : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
 
-        string[] pics = { "/school_project/Gallery/imgs/T1.jpg", "/../Gallery/imgs/T2.jpg", "/../Gallery/imgs/T3.jpg", "/../Gallery/imgs/T4.jpg", "/../Gallery/imgs/T5.jpg", "/../Gallery/imgs/T6.jpg", "/../Gallery/imgs/T7.jpg", "/../Gallery/imgs/T8.jpg", "/../Gallery/imgs/T9.jpg", "/../Gallery/imgs/T10.jpg", "/../Gallery/imgs/T11.jpg", "/../Gallery/imgs/T12.jpg", };
+        string[] pics = { "/school_project/Gallery/imgs/T1.jpg", "/school_project/Gallery/imgs/T2.jpg", "/school_project/Gallery/imgs/T3.jpg", "/school_project/Gallery/imgs/T4.jpg", "/school_project/Gallery/imgs/T5.jpg", "/school_project/Gallery/imgs/T6.jpg", "/school_project/Gallery/imgs/T7.jpg", "/school_project/Gallery/imgs/T8.jpg", "/school_project/Gallery/imgs/T9.jpg", "/school_project/Gallery/imgs/T10.jpg", "/school_project/Gallery/imgs/T11.jpg", "/school_project/Gallery/imgs/T12.jpg", };
+        String br = "<br />";
+        String tenSpace = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+
 
         for (int i = 0; i < 12; i++)
         {
-            str += "<img src=" + pics[i] + " alt=placeholder height=100 width=100>";
+            if (i % 3 == 0)
+            {
+
+                str += br;
+
+            }
+            str += "<img src=" + pics[i] + " alt=placeholder height=150 width=150>";
+            str += tenSpace;
         }
 
 
